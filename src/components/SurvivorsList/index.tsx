82947cb01b261e243b8ec4bf6e29747ad9fa503a
italofelipe <felipe.italo@hotmail.com>
2022-06-12 17:34:15 -0300

    feat: implemented search feature at the survivorslist component

--- src/components/SurvivorsList/index.tsx
+++ src/components/SurvivorsList/index.tsx
@@ -1,4 +1,14 @@
-import { Text } from "../../styles/styles";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FormEvent, useState } from "react";
+import {
+  Button,
+  Form,
+  FormContainer,
+  Input,
+  InputLabel,
+  Text
+} from "../../styles/styles";
 import { Aside, SurvivorsItem } from "./styles";
 type SurvivorsListProps = {
   survivors: Survivors[];
@@ -6,20 +16,70 @@ type SurvivorsListProps = {
 };
 
 const SurvivorsList = ({ onSelect, survivors }: SurvivorsListProps) => {
+  const [searchInput, setSearchInput] = useState<string>("");
+  const [filteredSurvivors, setFilteredSurvivors] = useState<Survivors | null>(
+    null
+  );
+
+  const resetInputAndFilter = () => {
+    setSearchInput("");
+    setFilteredSurvivors(null);
+  };
+
+  const filterSurvivor = () => {
+    const filter = survivors.find((survivor) =>
+      survivor.name.toLocaleLowerCase().includes(searchInput)
+    );
+    console.log("Filtro", filter);
+    setFilteredSurvivors(filter!);
+    if (searchInput === "") resetInputAndFilter();
+    return filter;
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    filterSurvivor();
+  };
   return (
     <Aside>
-      <h3>Survivors</h3>
-      {survivors.map((survivor) => (
-        <SurvivorsItem
-          data-testid={`survivor-item-${survivor.id}`}
-          key={survivor.id}
-          onClick={() => onSelect(survivor)}
-        >
+      <Form onSubmit={(e) => handleSubmit(e)}>
+        <InputLabel htmlFor="search-survivor">Search</InputLabel>
+        <FormContainer>
+        <Input
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+            onBlur={() => filterSurvivor()}
+            id="search-survivor"
+          />
+          <FontAwesomeIcon
+            width={"1rem"}
+            icon={faTrash}
+            onClick={() => resetInputAndFilter()}
+          />
+          
+        </FormContainer>
+
+        <Button type="submit">Procurar</Button>
+      </Form>
+      {survivors && !filteredSurvivors ? (
+        survivors.map((survivor) => (
+          <SurvivorsItem
+            data-testid={`survivor-item-${survivor.id}`}
+            key={survivor.id}
+            onClick={() => onSelect(survivor)}
+          >
+            <Text alignment="center" color="green">
+              {survivor.name}
+            </Text>
+          </SurvivorsItem>
+        ))
+      ) : (
+        <SurvivorsItem onClick={() => onSelect(filteredSurvivors!)}>
           <Text alignment="center" color="green">
-            {survivor.name}
+            {filteredSurvivors!.name}
           </Text>
         </SurvivorsItem>
-      ))}
+      )}
     </Aside>
   );
 };
--- src/components/SurvivorsList/styles.ts
+++ src/components/SurvivorsList/styles.ts
@@ -1,11 +1,15 @@
 import styled from "styled-components";
+import { mediaQueries } from "../../styles/mediaQueries";
 
 const Aside = styled.aside`
-  width: 30vw;
+  width: 40vw;
   height: 100vw;
   display: flex;
   flex-flow: column;
   align-items: baseline;
+  @media (min-width: ${mediaQueries.xsMin}) and (max-width: ${mediaQueries.mdMin}) {
+    width: 100%;
+  }
 `;
 
 const SurvivorsItem = styled.div`
@@ -16,3 +20,4 @@ export {
   Aside,
   SurvivorsItem
 };
+
