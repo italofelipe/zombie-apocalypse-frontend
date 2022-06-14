6fc6253e2d026d4ed1eb6545d23fbe5aa3c1c863
Italo Felipe <felipe.italo@hotmail.com>
2022-06-14 15:11:53 -0300

    feat: created modal to warn wether an infection gone well or not

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,38 @@
+import { faClose } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Text } from "../../styles/styles";
+import { ModalHeader, ModalInner, StyledModal } from "./styles";
+type ModalProps = {
+  handleDisplay: () => void;
+  context: "success" | "fail";
+  isOpen: boolean;
+};
+
+const Modal = ({ context, handleDisplay, isOpen }: ModalProps) => {
+  return (
+    <StyledModal isOpen={isOpen}>
+      <ModalInner>
+        <ModalHeader>
+          <FontAwesomeIcon
+            width={"1rem"}
+            icon={faClose}
+            onClick={() => handleDisplay()}
+            title="Limpar busca"
+          />
+        </ModalHeader>
+        {context === "success" ? (
+          <Text alignment="center" color="green">
+            O sobrevivente foi infectado e em breve, se juntará a horda de
+            zumbis!
+          </Text>
+        ) : (
+          <Text alignment="center" color="green">
+            Houve um erro ao infectar esse sobrevivente.
+          </Text>
+        )}
+      </ModalInner>
+    </StyledModal>
+  );
+};
+
+export default Modal;
