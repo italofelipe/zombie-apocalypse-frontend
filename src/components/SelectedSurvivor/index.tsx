08456b5c26197dd7b4ef5af68cb655888ec7780d
Italo Felipe <felipe.italo@hotmail.com>
2022-06-14 15:13:36 -0300

    feat: selected survivor component improved

--- src/components/SelectedSurvivor/index.tsx
+++ src/components/SelectedSurvivor/index.tsx
@@ -1,25 +1,105 @@
-import { Text } from "../../styles/styles";
-
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { axiosPut } from "../../services";
+import { InputLabel, Text } from "../../styles/styles";
+import Modal from "../Modal/Modal";
+import { Checkbox, SurvivorCard, SurvivorStats } from "./styles";
+const MaleImage = "/assets/man_image.svg";
+const FemaleImage = "/assets/woman_image.svg";
+const ZombieImage = "/assets/zombie_image.svg";
 
 type SelectedSurvivorProps = {
   survivor: Survivors;
+  onInfectSurvivor: () => void;
 };
 
-const SelectedSurvivor = ({ survivor }: SelectedSurvivorProps) => {
-  return (
-    <section>
-      <h1>{survivor.name}</h1>
+const SelectedSurvivor = ({
+  onInfectSurvivor,
+  survivor,
+}: SelectedSurvivorProps) => {
+  const [modalOpened, setModalOpened] = useState(false);
+  const [modalContext, setModalContext] = useState<"fail" | "success">("fail");
+  const [infectedSurvivor, setInfectedSurvivor] = useState<Survivors | null>(
+    null
+  );
 
-      <div>
-        <p>Skills:</p>{" "}
-        {survivor.skills.map((skill) => (
-          <div key={skill.rate}>
-            <Text alignment="center" color="green">{skill.skill}</Text>
-            <Text alignment="center" color="green">{skill.rate}</Text>
-          </div>
-        ))}
-      </div>
-    </section>
+  const handleInfect = (survivor: Survivors) => {
+    axiosPut<{
+      id: string;
+      name: string;
+      skills: Skills[];
+      isInfected: boolean;
+      gender: "male" | "female";
+    }>({
+      config: { path: "/survivors", params: survivor.id },
+      body: {
+        ...survivor,
+        isInfected: true,
+      },
+    })
+      .then((response) => {
+        onInfectSurvivor();
+        setModalContext("success");
+        setInfectedSurvivor(response.data);
+        setModalOpened(true);
+      })
+      .catch(() => {
+        setModalOpened(true);
+        setModalContext("fail");
+      });
+  };
+  useEffect(() => {
+    setInfectedSurvivor(survivor);
+  }, [survivor]);
+
+  return (
+    <>
+      {infectedSurvivor && (
+        <SurvivorCard>
+          <Text alignment="center" color="orange">
+            {infectedSurvivor.name}
+          </Text>
+          <Image
+            src={
+              infectedSurvivor.isInfected || infectedSurvivor?.isInfected
+                ? ZombieImage
+                : infectedSurvivor.gender === "female"
+                ? FemaleImage
+                : MaleImage
+            }
+            alt="Image of your survivor"
+            width={400}
+            height={400}
+          />
+          <p>Skills:</p>
+          {infectedSurvivor.skills.map((skill) => (
+            <SurvivorStats key={skill.rate}>
+              <Text alignment="center" color="green">
+                {skill.skill}
+              </Text>
+              <Text alignment="center" color="green">
+                {skill.rate}
+              </Text>
+            </SurvivorStats>
+          ))}
+
+          {!infectedSurvivor.isInfected && (
+            <InputLabel>Deseja infectar {infectedSurvivor.name}?</InputLabel>
+          )}
+          <Checkbox
+            disabled={infectedSurvivor.isInfected}
+            type="checkbox"
+            onChange={() => handleInfect(infectedSurvivor)}
+            checked={infectedSurvivor?.isInfected}
+          />
+          <Modal
+            context={modalContext}
+            isOpen={modalOpened}
+            handleDisplay={() => setModalOpened(!modalOpened)}
+          />
+        </SurvivorCard>
+      )}
+    </>
   );
 };
 export default SelectedSurvivor;
